868c24dd15fdf271db687580dc2240459805a337
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-03-06 05:22:59 +0600

    static file thik korte hobe

--- portfolio/justin/justin/settings.py
+++ portfolio/justin/justin/settings.py
@@ -113,10 +113,20 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
+import os 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...) 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+# Static files (CSS, JavaScript, Images) 
+STATIC_URL = '/static/' 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Corrected line 
+STATICFILES_DIRS = [ 
+os.path.join(BASE_DIR, 'static'),
+os.path.join(BASE_DIR, 'media'),
+]
+# Media files (uploads) 
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
